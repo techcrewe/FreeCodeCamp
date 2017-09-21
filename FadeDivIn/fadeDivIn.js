edbe66bf8870ffa73609d52dde6516306d454d81
Steve <sjcrewe@yahoo.com>
2017-09-21 20:36:21 +0100

    first commit of fade div in test files

---
+++ FadeDivIn/fadeDivIn.js
@@ -0,0 +1,13 @@
+$(window).scroll(function () {
+    console.log($(window).scrollTop());
+    var topDivHeight = $(".topdiv").height();
+    var viewPortSize = $(window).height();
+    
+    var triggerAt = 150;
+    var triggerHeight = (topDivHeight - viewPortSize) + triggerAt;
+
+    if ($(window).scrollTop() >= triggerHeight) {
+        $('.fadethisdiv').css('visibility', 'visible').hide().fadeIn();
+        $(this).off('scroll');
+    }
+});
